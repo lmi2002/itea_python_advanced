d9b7b800a1eb6721d7469858852307a46769317b
Vitaliy Lanin <vitaliy.lanin@exist.ua>
2018-10-12 16:44:49 +0300

    2018-10-12(2)

--- home_task_1.py
+++ home_task_1.py
@@ -1,26 +1,47 @@
 class LimitExceedError(Exception):
+    """
+    Raises when trying to push item when stack limit exceeded.
+    """
     pass
 
 
 class ZeroValue(Exception):
+    """
+    Raises when the number is zero
+    """
     pass
 
 
 class EmptyStackError(Exception):
+    """
+    Raises when trying to pull item from empty stack.
+    """
     pass
 
 
 class Stack:
+    """
+    Class <Stack>
+    Data structure based the principle of last in first out by default, it accepts values
+    data_type = object, limit = None
+    """
 
     def __init__(self, data_type=object, limit=None):
+        """
+        data_type: the type of data that will be stored in stack
+        limit: the amount of elements that should be stored in stack
+        """
         self.data_type = data_type
-        self.limit = int(limit)
+        self.limit = limit
         self.stack = []
 
         if limit == 0:
             raise ZeroValue("Number must be greater than zero!")
 
     def _push(self, obj):
+        """
+        Checks incoming element for type and limit and returns it
+        """
         if not isinstance(obj, self.data_type):
             raise TypeError
         elif self.limit < self.stack.__len__():
@@ -29,24 +50,42 @@ class Stack:
             return obj
 
     def push(self, obj):
+        """
+        Adds element to the stack
+        """
         obj = self._push(obj)
         self.stack.append(obj)
 
     def pull(self):
+        """
+        Removes an last element from the stack and returns it
+        """
         if self.stack.__len__() == 0:
             raise EmptyStackError("Stack is empty!")
         else:
             return self.stack.pop()
 
     def count(self):
+        """
+        Counts the number of elements in the stack and returns it
+        """
         return self.stack.__len__()
 
     def clear(self):
+        """
+        Removes all elements from the stack
+        """
         self.stack.clear()
 
     @property
     def type(self):
+        """
+        Adds an object to the property
+        """
         return self.data_type
 
     def __str__(self):
+        """
+        Returns a string with an object of type
+        """
         return 'Stack<{}>'.format(str(self.type)[7:-1])
--- test_home_task_1.py
+++ test_home_task_1.py
@@ -2,46 +2,71 @@ import pytest
 
 from home_task_1 import Stack
 
+"""
+Test to test class methods Stack:
+usage $ python -m pytest test_home_task_1.py
+"""
+
 
 def test_init_default_args():
+    """
+    Checking the default values type
+    """
     st = Stack()
     assert st.limit is None
     assert st.data_type is object
-    assert st.limit is int
 
 
 def test_check_type_el():
-    st = Stack(data_type=int, limit=2)
-    assert st.push('str')
+    """
+    Check of the method type
+    """
+    st = Stack(data_type=str, limit=2)
+    st.push('str')
+    assert type(st.stack[0]) == str
 
 
 def test_check___str__():
+    """
+    Check of the method ___str__
+    """
     st = Stack(data_type=int, limit=2)
     assert st.__str__() == "Stack<'int'>"
 
 
 def test_check_limit():
+    """
+    Check of the method limit
+    """
     st = Stack(data_type=int, limit=2)
     st.stack = [1, 2]
-    assert st.push(2)
+    st.push(2)
+    assert st.stack.__len__() == 3
 
 
 def test_check_pull():
+    """
+    Check of the method pull
+    """
     st = Stack(data_type=int, limit=2)
     st.stack = [1, 2]
     assert st.pull() == 2
-    st.stack = []
-    assert st.pull()
 
 
 def test_check_count():
+    """
+    Check of the method count
+    """
     st = Stack(data_type=int, limit=2)
-    stack = st.stack[1, 2]
-    assert stack.count() == 2
+    st.stack = [1, 2]
+    assert st.count() == 2
 
 
 def test_check_clear():
+    """
+    Check of the method clear
+    """
     st = Stack(data_type=int, limit=2)
     st.stack = [1, 2]
     st.clear()
-    assert not st.stack
+    assert st.stack.__len__() == 0
