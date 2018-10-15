bcc8357ad461cd0efa07a984c9583181f4e8ba74
lmi2002 <creman2002@gmail.com>
2018-10-15 22:12:25 +0300

    2018-10-15

--- home_task_2.py
+++ home_task_2.py
@@ -2,19 +2,17 @@ import os
 
 
 def monitor(cat_write, cat_result, cat_err):
-
     tuple_path_files = (os.walk(os.path.abspath('catalogs')))
     list_file = [file[2] for file in tuple_path_files]
-    print(list_file[2])
-    print(os.path.abspath('catalog_result_files'))
+    t = list_file[3]
+    r = list(map(lambda file: file if file.split('.')[1] == 'txt' else remove_file_catalog_error(cat_write, cat_err, file), list_file[3]))
+    print(r)
 
-
-def remove_file_catalog_error():
-    r = os.path.join(os.path.abspath('catalog_result_files'), 'r.txt')
-    d = os.path.join(os.path.abspath('catalog_error_files'), 'r.txt')
-    os.rename(r, d)
+def remove_file_catalog_error(cat_write, cat_err, file):
+    src = os.path.join(os.path.abspath('catalogs'), cat_write, file)
+    dst = os.path.join(os.path.abspath('catalogs'), cat_err, file)
+    os.replace(src, dst)
 
 
 
 monitor('catalog_writing_files', 'catalog_result_files', 'catalog_error_files')
-
